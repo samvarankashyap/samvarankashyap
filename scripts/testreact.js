85f2e911931da7e36d0d6d3e8b975854add91481
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2017-09-05 11:03:01 -0400

    Added scripts folder with test script file

---
+++ scripts/testreact.js
@@ -0,0 +1,10 @@
+/*** @jsx React.DOM */
+var realReact = React.createClass({
+    render: function() {
+    return (<h2>Greetings, This is rendered from React hahaha!</h2>);
+    }
+});
+ReactDOM.render(
+   React.createElement(realReact, null),
+   document.getElementById('content')
+);
