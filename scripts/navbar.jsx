189fbe0815747ad939dd1e1a558c12b9753efd41
samvarankashyap <samvaran.kashyap@gmail.com>
2017-10-13 14:39:55 -0400

    Added a sample navbar to look how it works

---
+++ scripts/navbar.jsx
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+var NavBar = React.createClass({
+  generateItem: function(item){
+    return <NavBarItem text={item.text} url={item.url} submenu={item.submenu} />
+  },
+  render: function() {
+    var items = this.props.items.map(this.generateItem);
+    return (
+      <ul className="menu">
+      {items}
+      </ul>
+    );
+  }
+})
