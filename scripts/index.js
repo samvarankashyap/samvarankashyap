948f07f91580805f136dbf3a4a4fa4b05d581933
samvarankashyap <samvaran.kashyap@gmail.com>
2017-10-13 14:46:33 -0400

    moved around nav bar

--- scripts/index.js
+++ scripts/index.js
@@ -8,4 +8,8 @@ ReactDOM.render(
    React.createElement(realReact, null),
    document.getElementById('content')
 );
+window.onload = React.renderComponent(
+        <NavBar items={data} />,
+        document.querySelector('nav')
+);
 ReactDOM.render(<App />, document.getElementById('root'));
